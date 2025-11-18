a17054be26d28d859c13451f34ffc11b54910473
infra-team333 <244766478+infra-team333@users.noreply.github.com>
2025-11-18 13:41:21 -0500

    Update globalOverrides.js
    Updating method again, previous method did not work well with ITSM.

--- TDX-Overrides/Sandbox/globalOverrides.js
+++ TDX-Overrides/Sandbox/globalOverrides.js
@@ -1,7 +1,7 @@
 // globalOverrides.js
 
-export const css = `
-/* ====== Hide buttons named HideMe3000 ====== */
+window.globalCSS = `
+/* Hide buttons named HideMe3000 */
 #btnSubmit[value="HideMe3000"] {
   display: none !important;
 }
